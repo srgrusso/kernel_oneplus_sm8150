a8cc1ae03886084d92df53750ea59ebb0afa699b
Shubham Talekar <stalek@codeaurora.org>
2021-11-17 23:09:08 +0530

    drm: msm: dp: prevent calling client_register multiple times
    Prevent calling drm_client_register function if it is
    already registered.
    
    Change-Id: I622a79954859561fb0bdc7af225c32d4c7cfdc1f
    Signed-off-by: Shubham Talekar <stalek@codeaurora.org>

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -27,6 +27,7 @@
 #include <linux/soc/qcom/max20328.h>
 #endif /* OPLUS_FEATURE_DP_MAX20328 */
 
+#include <drm/drm_client.h>
 #include "sde_connector.h"
 
 #include "msm_drv.h"
@@ -692,6 +693,10 @@ static int dp_display_send_hpd_notification(struct dp_display_private *dp)
 
 	if (!dp_display_framework_ready(dp)) {
 		pr_debug("%s: dp display framework not ready\n", __func__);
+		if (!dp->dp_display.is_bootsplash_en) {
+			dp->dp_display.is_bootsplash_en = true;
+			drm_client_dev_register(dp->dp_display.drm_dev);
+		}
 		return ret;
 	}
 
