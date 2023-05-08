d798c07b0d53a2e98275aba3c5b6f6f0ba775e7c
ShevT <evgeny.shishkov@gmail.com>
2023-05-08 20:57:22 +0300

    Revert "ANDROID: cgroup: Fix for a partially backported patch"
    This reverts commit 63da7267cb307464e65a34346f5295b635347739.
    
    Change-Id: I59da402cbb732c6a5a16cccfcc08e6bf37a05f58

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -3478,7 +3478,6 @@ static int cgroup_cpu_pressure_show(struct seq_file *seq, void *v)
 static ssize_t cgroup_pressure_write(struct kernfs_open_file *of, char *buf,
 					  size_t nbytes, enum psi_res res)
 {
-	struct cgroup_file_ctx *ctx = of->priv;
 	struct psi_trigger *new;
 	struct cgroup *cgrp;
 
@@ -3490,7 +3489,7 @@ static ssize_t cgroup_pressure_write(struct kernfs_open_file *of, char *buf,
 	cgroup_kn_unlock(of->kn);
 
 	/* Allow only one trigger per file descriptor */
-	if (ctx->psi.trigger) {
+	if (of->priv) {
 		cgroup_put(cgrp);
 		return -EBUSY;
 	}
@@ -3501,7 +3500,7 @@ static ssize_t cgroup_pressure_write(struct kernfs_open_file *of, char *buf,
 		return PTR_ERR(new);
 	}
 
-	smp_store_release(&ctx->psi.trigger, new);
+	smp_store_release(&of->priv, new);
 	cgroup_put(cgrp);
 
 	return nbytes;
@@ -3531,16 +3530,12 @@ static ssize_t cgroup_cpu_pressure_write(struct kernfs_open_file *of,
 static unsigned int cgroup_pressure_poll(struct kernfs_open_file *of,
 					 poll_table *pt)
 {
-	struct cgroup_file_ctx *ctx = of->priv;
-
-	return psi_trigger_poll(&ctx->psi.trigger, of->file, pt);
+	return psi_trigger_poll(&of->priv, of->file, pt);
 }
 
 static void cgroup_pressure_release(struct kernfs_open_file *of)
 {
-	struct cgroup_file_ctx *ctx = of->priv;
-
-	psi_trigger_destroy(ctx->psi.trigger);
+	psi_trigger_destroy(of->priv);
 }
 
 bool cgroup_psi_enabled(void)
