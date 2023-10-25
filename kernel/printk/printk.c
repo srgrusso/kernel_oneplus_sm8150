b6682007e00c2cdbe44cc5c8a933e3e3c97c5b84
ShevT <evgeny.shishkov@gmail.com>
2023-10-25 17:37:30 +0300

    treewide: Clean up oplus POWERINFO_FTM feature
    Change-Id: I1f9746c04b86b7d1f322840de2d6449602f34b69

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -64,24 +64,6 @@
 #include <linux/time.h>
 #endif
 
-#ifdef OPLUS_FEATURE_POWERINFO_FTM
-#include <soc/oplus/system/boot_mode.h>
-static bool __read_mostly printk_disable_uart = true; /*set true avoid early console output*/
-static int __init printk_uart_disabled(char *str)
-{
-	if (str[0] == '1')
-		printk_disable_uart = true;
-	else
-		printk_disable_uart = false;
-	return 0;
-}
-early_param("printk.disable_uart", printk_uart_disabled);
-
-bool oem_disable_uart(void)
-{
-	return printk_disable_uart;
-}
-#endif /*OPLUS_FEATURE_POWERINFO_FTM*/
 #ifdef CONFIG_EARLY_PRINTK_DIRECT
 extern void printascii(char *);
 #endif
@@ -1767,14 +1749,6 @@ static void call_console_drivers(const char *ext_text, size_t ext_len,
 		return;
 
 	for_each_console(con) {
-#ifdef OPLUS_FEATURE_POWERINFO_FTM
-		if ((con->flags & CON_CONSDEV) &&
-				(printk_disable_uart ||
-				get_boot_mode() == MSM_BOOT_MODE__FACTORY ||
-				get_boot_mode() == MSM_BOOT_MODE__RF ||
-				get_boot_mode() == MSM_BOOT_MODE__WLAN))
-			continue;
-#endif /*VENDOR_EDIT*/
 		if (exclusive_console && con != exclusive_console)
 			continue;
 		if (!(con->flags & CON_ENABLED))
